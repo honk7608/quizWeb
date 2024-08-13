ba3e25e4c417357ae40660fe2700a1d97fc12112
honk7608 <honk7608@gmail.com>
2024-08-14 01:06:16 +0900

    퀴즈 기능 완성
    - 스크립트 파일 분리
    - 문제 풀이, 결과 확인, 재시도 기능 추가

---
+++ quiz/quiz.js
@@ -0,0 +1,227 @@
+/**
+ * 퀴즈 종합 함수
+ * @param {{eng: string[], kor: string[], mean: string, submit: string}[]} words 
+ */
+function main(words) {
+ 
+    // 기본 세팅 (변경 X)
+
+    var gridEl = document.getElementsByClassName("gridD")[0]
+
+    var problemType = "";
+    var ansType = "";
+    var showMean = false;
+    var showTime = false;
+    var mode = 1; 
+
+    // 추가 세팅 (재시도 시 변경 가능)
+    var allCnt = words.length;
+    var correctWords = [], wrongWords = [];
+    var index = 1;
+
+    /** @type {Element} */
+    var probEl
+    /** @type {Element} */
+    var inputEl
+    /** @type {Element} */
+    var meanEl
+    /** @type {Element} */
+    var ansFormEl
+    /** @type {Element} */
+    var continueButtonEl
+    /** @type {Element} */
+    var doneButtonEl
+    /** @type {Element} */
+    var progressEl;
+    /** @type {Element} */
+    var retryButtonEl
+    /** @type {Element} */
+    var endButtonEl
+
+    function enterNextCallback(e) {
+        if(e.key == "Enter") {e.preventDefault; next();}
+    }
+
+    function enterCheckAnsCallback(e) {
+        if(e.key == "Enter") {e.preventDefault; checkAns();}
+    }
+
+    /**
+     * 다음 문제 생성
+     * @param {Event} e
+     * @returns {none}
+     */
+    function next(e) {
+        if(e) e.preventDefault();
+        if(words.length == 0) return results();
+        
+        // 돌려놓기
+        inputEl.disabled = false;
+        inputEl.value = ''
+        inputEl.className = null;
+
+        doneButtonEl.style.display = 'block';
+        ansFormEl.onsubmit = checkAns;
+        continueButtonEl.style.display = 'none';
+        continueButtonEl.onclick = null;
+        document.removeEventListener("keypress", enterNextCallback)
+        document.addEventListener("keypress", enterCheckAnsCallback)
+
+        // 새 문제 세팅
+        index = Math.floor(Math.random() * words.length);
+        
+        probEl.innerHTML = ''
+        probEl.innerHTML = words[index][problemType].join(',&nbsp;');
+        if(showMean) meanEl.innerHTML = words[index].mean;
+    }
+
+    /**
+     * 정답 확인
+     * @param {Event} e 
+     */
+    function checkAns(e) {
+        if(e) e.preventDefault();
+
+        inputEl.disabled = true;
+
+        doneButtonEl.style.display = 'none';
+        ansFormEl.onsubmit = null;
+        continueButtonEl.style.display = 'block';
+        continueButtonEl.onclick = next;
+        document.removeEventListener("keypress", enterCheckAnsCallback)
+        document.addEventListener("keypress", enterNextCallback)
+
+        // 정답 확인
+        inputAns = inputEl.value
+        rightAns = words[index][ansType]
+        words[index].submit = inputAns;
+        if(rightAns.includes(inputAns)) {inputEl.className = "correct"; correctWords.push(words.splice(index, 1)[0]);}
+        else {inputEl.className = "wrong"; probEl.innerHTML += `&nbsp;<span class='corrAns'>&nbsp;${words[index][ansType]}&nbsp;</span>`; wrongWords.push(words.splice(index, 1)[0]);}
+
+        // 진척도 표시
+        progressEl.innerHTML = `${allCnt}개 단어 중 &nbsp;<span class="corrCnt">&nbsp;${correctWords.length}개 정답&nbsp;</span>&nbsp;<span class="wrongCnt">&nbsp;${wrongWords.length}개 오답&nbsp;</span>&nbsp;<span class="leftCnt">&nbsp;${words.length}개 남음&nbsp;</span>`
+        if(words.length == 0) {results()}
+    }
+
+    /**
+     * 시작
+     */
+    function start() {
+        gridEl.innerHTML = 
+        `<div id="progress">
+            ${allCnt}개 단어 중 &nbsp;<span class="corrCnt">&nbsp;0개 정답&nbsp;</span>&nbsp;<span class="wrongCnt">&nbsp;0개 오답&nbsp;</span>&nbsp;<span class="leftCnt">&nbsp;0개 남음&nbsp;</span>
+        </div>
+        <div class="problem">
+            <div class="prob"> 문제 </div>
+            ${showMean?"<div class='mean'> 영영풀이 </div>":""}
+        </div>
+        <div class="ans">
+            <form id="ansForm">
+                <div class="guide">${(mode == 2)?"뜻":"영단어"}</div>
+                <input type="text" name="ans" id="ans" id="ansForm" autocomplete="off">
+                <input type="submit" name="done" id="done" value="정답 제출하기" id="ansForm">
+            </form>
+            <button id="continue">계속하기</button>
+        </div>`
+        probEl = document.getElementsByClassName("prob")[0];
+        meanEl = document.getElementsByClassName("mean")[0];
+        ansFormEl = document.getElementById("ansForm");
+        ansFormEl.onsubmit = checkAns;
+        inputEl = document.getElementById("ans");
+        doneButtonEl = document.getElementById("done");
+        continueButtonEl = document.getElementById("continue");
+        continueButtonEl.onclick = next;
+        progressEl = document.getElementById("progress");
+
+        next();
+    };
+
+    /**
+     * 결과 확인 및 재시도 결정
+     */
+    function results() {
+        gridEl.innerHTML = 
+        `<div class="cnt">
+            <div class="correct"> ${correctWords.length} / ${allCnt}</div>
+            <div class="wrong"> ${wrongWords.length} / ${allCnt}</div>
+            <a id="end" href="../index.html">메인 화면으로 돌아가기</a>
+            ${(wrongWords.length)?"<button id='continue'>틀린 단어만 다시하기</button>":''}
+        </div>`
+        listText = 
+        `<div class="list">
+            <table><tbody>
+                <tr class="legends Head"><td>문제</td><td>정답</td><td>제출 답안</td></tr>
+                <tr class="correct Head"><td colspan="3">맞춘 단어</td></tr>`
+        for(i in correctWords) {
+            listText += `<tr class="correct Li ${(i==correctWords.length-1)?'last':''}"><td>${correctWords[i][problemType].join(', ')}</td><td>${correctWords[i][ansType].join(', ')}</td><td>${correctWords[i].submit}</td></tr>`;
+        }
+        listText += 
+        `       <tr class="wrong Head"><td colspan="3">틀린 단어</td></tr>`
+        for(i in wrongWords) {
+            listText += `<tr class="wrong Li ${(i==wrongWords.length-1)?'last':''}"><td>${wrongWords[i][problemType].join(', ')}</td><td>${wrongWords[i][ansType].join(', ')}</td><td>${wrongWords[i].submit}</td></tr>`;
+        }
+        listText += 
+        `   </tbody></table>
+        </div>` 
+        gridEl.innerHTML += listText  
+
+        continueButtonEl = document.getElementById('continue')
+        continueButtonEl.onclick = (e) => {
+            words = wrongWords;
+            correctWords = [];
+            wrongWords = [];
+            allCnt = words.length;
+
+            start()
+        }
+    }
+
+    /**
+     * 옵션 설정
+     */
+    function options() {
+        gridEl.innerHTML = 
+        `<form class="options" id="options">
+            <div>
+                <label for="kte"> <input type="radio" name="mode" id="kte" value="1" form="options" required> 뜻 보고 단어 맞추기</label>
+                <label for="etk"> <input type="radio" name="mode" id="etk" value="2" form="options"> 단어 보고 뜻 맞추기</label>
+            </div>
+
+            <div>
+                <label for="mean"> <input type="checkbox" name="mean" id="mean" form="options"> 영영풀이 보이기</label>
+            </div>
+
+            <div>
+                <label for="timer"> <input type="checkbox" name="timer" id="timer" form="options"> 타이머 보이기</label>
+            </div>
+
+            <input type="submit" value="시작하기" id="done" form="options">
+        </div>`;
+        
+        // Option Submit Event
+        
+        optionFormEl = document.getElementById("options");
+        optionFormEl.addEventListener("submit", (e) => {
+            e.preventDefault();
+            
+            const formData = new FormData(optionFormEl);
+            
+            mode = formData.get("mode")
+            if(mode == 1) {problemType = 'kor'; ansType = 'eng';}
+            else {problemType = 'eng'; ansType = 'kor';}
+            showMean = formData.get("mean")=='true';
+            showTime = formData.get("timer")=='false';
+            
+            start();
+        })
+        
+        optionFormEl.addEventListener("formdata", (e) => {
+            const formData = e.formData;
+            formData.set("mode", formData.get("mode"));
+            formData.set("mean", Boolean(formData.get("mean")));
+            formData.set("timer", Boolean(formData.get("timer")));
+        });
+    }
+
+    options()
+}
---
+++ quiz/김print.js
@@ -0,0 +1,23 @@
+var words = [
+    {"eng": ["case"], "kor": ["사건"], "mean": "a crime that the police are trying to solve,<br/>or a problem that someone has"}, 
+    {"eng": ["rush"], "kor": ["서두르다"], "mean": "to hurry in order to get somewhere very quickly"}, 
+    {"eng": ["obviously"], "kor": ["명백하게"], "mean": "in a way that is clear for almost anyone to see or understand"}, 
+    {"eng": ["desperately"], "kor": ["절망적으로"], "mean": "in a very worried or angry way"}, 
+    {"eng": ["scandal"], "kor": ["스캔들"], "mean": "talk or reports in the newspapers about shocking events "}, 
+    {"eng": ["break someone's heart"], "kor": ["마음을 찢어지게 하다"], "mean": "to upset someone very much, especially by doing something bad "}, 
+    {"eng": ["charge"], "kor": ["기소하다", "고발하다", "비난하다"], "mean": "to accuse someone officially of committing a crime.<br/>If you commit a crime, you do something legally wrong."}, 
+    {"eng": ["investigate"], "kor": ["조사하다"], "mean": "to try to find out the facts about something<br/>in order to learn the truth about it "}, 
+    {"eng": ["fierce"], "kor": ["사나운"], "mean": "very angry, or ready to attack"}, 
+    {"eng": ["lung"], "kor": ["폐"], "mean": "one of the two organs in your chest<br/>that fill with air when you breathe "}, 
+    {"eng": ["legal"], "kor": ["법률적인"], "mean": "relating to the law or lawyers"}, 
+    {"eng": ["search"], "kor": ["찾다"], "mean": "to try to find something or someone by looking carefully"}, 
+    {"eng": ["suspect"], "kor": ["용의자"], "mean": "someone who the police believe may have committed a crime.<br/>If someone suspects someone of something,<br/>they believe that someone has done something,<br/>usually something bad."}, 
+    {"eng": ["district"], "kor": ["구역"], "mean": "one of the areas into<br/>which a town or country is divided for official purposes"}, 
+    {"eng": ["firm"], "kor": ["회사"], "mean": "a business or company"}, 
+    {"eng": ["except for"], "kor": ["~을 제외하고", "~를 제외하고"], "mean": "used for introducing the only person, thing or fact<br/>that is not included in your main statement"}, 
+    {"eng": ["fear the worst"], "kor": ["최악의 경우를 생각하다"], "mean": "to fear that something very bad has happened to someone or that they could be dead"}, 
+    {"eng": ["arrest"], "kor": ["체포하다"], "mean": "If the police arrest someone,<br/>they take that person to a police station<br/>because they believe he or she has committed a crime."}, 
+    {"eng": ["protect"], "kor": ["보호하다"], "mean": "to keep someone or something safe<br/>from harm, injury, damage or loss"}
+];
+
+main(words);
