6bcfb6a42c5b486fe54b54952413676387cc3f57
honk7608 <honk7608@gmail.com>
2024-10-08 00:35:30 +0900

    3번 프린트 적용

---
+++ quiz/박print3.js
@@ -0,0 +1,15 @@
+var words = [
+    {"korAccept": [], "korMain": ["Fill in the (???)"], "eng": ["proof"], "mean": "From seven million miles away, photographic (???) that NASA's DART mission ()."}, 
+    {"korAccept": [], "korMain": ["Fill in the (???)"], "eng": ["worked"], "mean": "From seven million miles away, photographic () that NASA's DART mission (???)."}, 
+    {"korAccept": [], "korMain": ["Fill in the (???)"], "eng": ["big shove"], "mean": "The impact gave Dimorphos a (???), dramatically shortening its orbit even more than NASA had hoped."}, 
+    {"korAccept": [], "korMain": ["Fill in the (???)"], "eng": ["if it only"], "mean": "\"It was expected to be a huge success (???) slowed the orbit by about 10 minutes, but it actually slowed it by 32 minutes.\""}, 
+    {"korAccept": [], "korMain": ["Fill in the (???)"], "eng": ["caught the final"], "mean": "Travelling at 14,000 miles per hour, DART's nose camera (???) seconds before impact."}, 
+    {"korAccept": [], "korMain": ["Fill in the (???)"], "eng": ["poses no risk"], "mean": "Although the asteroid (???) to us, NASA is hoping it can one day use the same technique to divert a massive meteor on a collision course with Earth, a so-called planet killer like the one that () the dinosaurs 65 million years ago."}, 
+    {"korAccept": [], "korMain": ["Fill in the (???)"], "eng": ["killed off"], "mean": "Although the asteroid () to us, NASA is hoping it can one day use the same technique to divert a massive meteor on a collision course with Earth, a so-called planet killer like the one that (???) the dinosaurs 65 million years ago."}, 
+    {"korAccept": [], "korMain": ["Fill in the (???)"], "eng": ["enable this sort"], "mean": "\"Warning time is a really key here in order to (???) of asteroid deflection to potentially be used in the future.\""}, 
+    {"korAccept": [], "korMain": ["Fill in the (???)"], "eng": ["an imminent threat"], "mean": "NASA says it's not tracking any asteroid known to pose (???) to Earth, but there may be others it doesn't see."}, 
+    {"korAccept": [], "korMain": ["Fill in the (???)"], "eng": ["would have"], "mean": "Ideally, scientists (???) decades of warning to use a similar defelction technique and save humanity."}, 
+    {"korAccept": [], "korMain": ["Fill in the (???)"], "eng": ["the only one we have"], "mean": "\"All of us have a responsibility to protect our home planet. After all, it's (???).\""}
+    ];
+
+main(words);
